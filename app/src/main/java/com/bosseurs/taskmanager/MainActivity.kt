78724eb46b28e2856dca99891058f9bbfdd912a8
ayoubyte <ayoubboubkrioui2@gmail.com>
2023-03-05 01:08:57 +0100

    Practicing Jetpack Compose

---
+++ app/src/main/java/com/bosseurs/taskmanager/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.bosseurs.taskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bosseurs.taskmanager.ui.theme.TaskManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskManagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TaskManager()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TaskManager(){
+    val image = painterResource(R.drawable.ic_task_completed)
+    Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = image, null, modifier = Modifier.width(150.dp).height(150.dp))
+        Text(stringResource(R.string.all_task_completed), fontWeight = FontWeight.Bold, modifier=Modifier.padding(top = 24.dp, bottom = 8.dp))
+        Text(stringResource(id = R.string.nice_work), fontSize = 16.sp)
+
+    }
+}
